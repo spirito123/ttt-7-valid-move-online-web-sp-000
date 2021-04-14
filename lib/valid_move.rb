65fdd2c324f913e0bc5b110987bac71ee15f7382
Christopher Del Basso <ccdelbasso@gmail.com>
2021-04-14 17:36:38 -0400

    commit tree 41095958b4de56828595ed71c21794a97916d140

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,27 @@
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# input = gets.chomp
+
+# def input_to_index(input)
+#     input.to_i - 1
+# end
+
+# index = input_to_index
+
+
 # code your #valid_move? method here
+def valid_move?(board, index)
+    if index.between?(0,8) && !position_taken?(board, index)
+       return true
+    end
+ end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+       return false 
+    else
+       return true
+    end
+ end
